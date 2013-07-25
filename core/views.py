45faaa3d820ffa5568fbdf0fe18d3625e960227c
Matheus Lima <matheus.se@gmail.com>
2013-07-25 15:38:59 -0300

    aula 05

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
 
 from models import Pessoa
 
@@ -13,4 +14,16 @@ def lista_pessoas(request):
         context = {'pessoas': pessoas}
         return render(request, 'lista_pessoas.html', context)
     else:
+        raise Http404
+
+
+class ListaPessoas(View):
+    template_name = 'lista_pessoas.html'
+
+    def get(self, request):
+        pessoas = Pessoa.objects.all().order_by('nome')
+        context = {'pessoas': pessoas}
+        return render(request, 'lista_pessoas.html', context)
+
+    def post(self, request):
         raise Http404
--- curso/urls.py
+++ curso/urls.py
@@ -4,8 +4,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from core.views import ListaPessoas
+
 urlpatterns = patterns('',
     url(r'^$', 'core.views.lista_pessoas', name='home'),
-
+    url(r'^lista-pessoas/$', ListaPessoas.as_view(), name='lista_pessoas'),
     url(r'^admin/', include(admin.site.urls)),
 )
